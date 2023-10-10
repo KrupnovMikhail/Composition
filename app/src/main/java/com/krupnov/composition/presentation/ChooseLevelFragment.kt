4f1074203ba042d4f5500dc31d2e3df1061f26b5
Mikhail Krupnov <krupnovmn@gmail.com>
2023-10-10 22:19:58 +0300

    add jetPackNavigation logic

--- app/src/main/java/com/krupnov/composition/presentation/ChooseLevelFragment.kt
+++ app/src/main/java/com/krupnov/composition/presentation/ChooseLevelFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.krupnov.composition.R
 import com.krupnov.composition.databinding.FragmentChooseLevelBinding
 import com.krupnov.composition.domain.entity.Level
@@ -44,10 +45,10 @@ class ChooseLevelFragment : Fragment() {
     }
 
     private fun launchGameFragment(level: Level) {
-        requireActivity().supportFragmentManager.beginTransaction()
-            .replace(R.id.main_container, GameFragment.newInstance(level))
-            .addToBackStack(GameFragment.NAME)
-            .commit()
+        val args = Bundle().apply {
+            putParcelable(GameFragment.KEY_LEVEL, level)
+        }
+        findNavController().navigate(R.id.action_chooseLevelFragment_to_gameFragment, args)
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/krupnov/composition/presentation/GameFragment.kt
+++ app/src/main/java/com/krupnov/composition/presentation/GameFragment.kt
@@ -9,6 +9,7 @@ import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.krupnov.composition.R
 import com.krupnov.composition.databinding.FragmentGameBinding
 import com.krupnov.composition.domain.entity.GameResult
@@ -91,7 +92,7 @@ class GameFragment : Fragment() {
             binding.progressBar.secondaryProgress = it
         }
         viewModel.gameResult.observe(viewLifecycleOwner) {
-            launchFinishedFragment(it)
+            launchGameFinishedFragment(it)
         }
         viewModel.progressAnswers.observe(viewLifecycleOwner) {
             binding.tvAnswersProgress.text = it
@@ -118,17 +119,17 @@ class GameFragment : Fragment() {
         }
     }
 
-    private fun launchFinishedFragment(gameResult: GameResult) {
-        requireActivity().supportFragmentManager.beginTransaction()
-            .replace(R.id.main_container, GameFinishedFragment.newInstance(gameResult))
-            .addToBackStack(null)
-            .commit()
+    private fun launchGameFinishedFragment(gameResult: GameResult) {
+        val args = Bundle().apply {
+            putParcelable(GameFinishedFragment.KEY_GAME_RESULT, gameResult)
+        }
+        findNavController().navigate(R.id.action_gameFragment_to_gameFinishedFragment, args)
     }
 
     companion object {
 
         const val NAME = "GameFragment"
-        private const val KEY_LEVEL = "level"
+        const val KEY_LEVEL = "level"
 
         fun newInstance(level: Level): GameFragment {
             return GameFragment().apply {
