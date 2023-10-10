2c96a77c94eaf562d0b5902b0ca7a46350cca1c3
Mikhail Krupnov <krupnovmn@gmail.com>
2023-10-10 21:15:50 +0300

    add GameViewModelFactory.kt

--- app/src/main/java/com/krupnov/composition/presentation/GameFragment.kt
+++ app/src/main/java/com/krupnov/composition/presentation/GameFragment.kt
@@ -9,21 +9,19 @@ import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory
 import com.krupnov.composition.R
 import com.krupnov.composition.databinding.FragmentGameBinding
 import com.krupnov.composition.domain.entity.GameResult
-import com.krupnov.composition.domain.entity.GameSettings
 import com.krupnov.composition.domain.entity.Level
 
 class GameFragment : Fragment() {
 
     private lateinit var level: Level
+    private val viewModelFactory by lazy {
+        GameViewModelFactory(level, requireActivity().application)
+    }
     private val viewModel: GameViewModel by lazy {
-        ViewModelProvider(
-            this,
-            AndroidViewModelFactory.getInstance(requireActivity().application)
-        )[GameViewModel::class.java]
+        ViewModelProvider(this, viewModelFactory)[GameViewModel::class.java]
     }
 
     private val tvOptions by lazy {
@@ -58,7 +56,6 @@ class GameFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         observeViewModel()
         setClickListenersToOptions()
-        viewModel.startGame(level)
     }
 
     private fun setClickListenersToOptions() {
